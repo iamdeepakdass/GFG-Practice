c6c2889fc56dd184acc9f137c4227b91927e7a9d
Deepak Dass <105672156+iamdeepakdass@users.noreply.github.com>
2023-05-20 17:39:22 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Boolean Matrix/boolean-matrix.cpp
@@ -0,0 +1,96 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+
+
+class Solution
+{   
+    public:
+    //Function to modify the matrix such that if a matrix cell matrix[i][j]
+    //is 1 then all the cells in its ith row and jth column will become 1.
+    
+    void makeRowOne(int i, int c, vector<vector<int>> &matrix){
+        
+        for(int k=0; k<c; k++){
+            matrix[i][k] = 1;
+        }
+    }
+    
+    void makeColOne(int j, int r, vector<vector<int>> &matrix){
+        
+        for(int k=0; k<r; k++){
+            matrix[k][j] = 1;
+        }
+    }
+    
+    void booleanMatrix(vector<vector<int>> &matrix) 
+    {
+        // code here 
+        
+        int r = matrix.size();
+        int c = matrix[0].size();
+        
+        vector<int> row(r,0);
+        vector<int> col(c);
+        
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c; j++){
+                if(matrix[i][j] == 1){
+                    row[i] = -1;
+                    col[j] = -1;
+                }
+            }  
+        }
+        
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c; j++){
+                if(row[i] == -1 || col[j]){
+                    matrix[i][j] = 1;
+                }
+            }  
+        }
+        
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin>>t;
+    while(t--) 
+    {
+        int row, col;
+        cin>> row>> col;
+        vector<vector<int> > matrix(row); 
+        for(int i=0; i<row; i++)
+        {
+            matrix[i].assign(col, 0);
+            for( int j=0; j<col; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+        
+        Solution ob;
+        ob.booleanMatrix(matrix);
+
+
+        for (int i = 0; i < row; ++i)
+        {
+            for (int j = 0; j < col; ++j)
+            {
+                cout<<matrix[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
