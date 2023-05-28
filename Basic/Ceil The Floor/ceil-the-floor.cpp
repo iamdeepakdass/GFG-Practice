04be3edba05bd19a059786382dde249c810f7ccb
Deepak Dass <105672156+iamdeepakdass@users.noreply.github.com>
2023-05-28 20:37:10 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Ceil The Floor/ceil-the-floor.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+pair<int, int> getFloorAndCeil(int arr[], int n, int x);
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        auto ans = getFloorAndCeil(arr, n, x);
+        cout << ans.first << " " << ans.second << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+pair<int, int> getFloorAndCeil(int arr[], int n, int x) {
+    // code here
+    
+    int ceil = -1;
+    int floor = -1;
+    
+    for(int i=0; i<n; i++){
+        
+        if(arr[i] <= x){
+            floor = max(floor, arr[i]);
+        }
+        
+        if(arr[i] >= x){
+            if(ceil == -1){
+                ceil = arr[i];
+            }
+            ceil = min(ceil, arr[i]);
+        }
+        
+    }
+    
+    
+    return {floor, ceil};
+}
