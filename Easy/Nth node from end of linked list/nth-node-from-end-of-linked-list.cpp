03c14fb70449c8547e2cb4e5e1eb858f4c44e4f5
Deepak Dass <105672156+iamdeepakdass@users.noreply.github.com>
2023-05-28 12:29:29 +0530

    Updated Solution - GfG to GitHub

--- Easy/Nth node from end of linked list/nth-node-from-end-of-linked-list.cpp
+++ Easy/Nth node from end of linked list/nth-node-from-end-of-linked-list.cpp
@@ -1,4 +1,5 @@
 //{ Driver Code Starts
+//Initial Template for C++
 // C program to find n'th Node in linked list
 #include <stdio.h>
 #include <stdlib.h>
@@ -22,6 +23,55 @@ int getNthFromLast(struct Node* head, int n);
 
 
 /* Driver program to test above function*/
+
+// } Driver Code Ends
+/* struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+//Function to find the data of nth node from the end of a linked list.
+class Solution{
+public:
+    int getNthFromLast(Node *head, int m)
+    {
+           // Your code here
+           // let n be the length of the linked list
+        int n = 0;
+        Node *temp = head;
+       
+        while(temp != NULL){
+           n++;
+           temp = temp->next;
+        }    
+       
+        int k = 0;
+        temp = head;
+        int ans = -1;
+       
+        while(temp != NULL){
+           // n-k would give the Nth node from the end of the linked list
+           if(n-k == m){
+               ans = temp->data;
+           }
+           
+           temp = temp->next;
+           k++;
+        }
+       
+        return ans;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
 int main()
 {
   int T,i,n,l,k;
@@ -42,48 +92,9 @@ int main()
             tail->next = new Node(l);
             tail = tail->next;
         }
-
-    cout<<getNthFromLast(head, k)<<endl;
+    Solution obj;
+    cout<<obj.getNthFromLast(head, k)<<endl;
     }
     return 0;
 }
 // } Driver Code Ends
-
-
-/* struct Node {
-  int data;
-  struct Node *next;
-  Node(int x) {
-    data = x;
-    next = NULL;
-  }
-};
-*/
-
-//Function to find the data of nth node from the end of a linked list.
-int getNthFromLast(Node *head, int m)
-{
-       // Your code here
-       int n = 0;
-       
-       Node *temp = head;
-       
-       while(temp != NULL){
-           n++;
-           temp = temp->next;
-       }
-       
-       int k = 0;
-       temp = head;
-       int ans = -1;
-       while(temp != NULL){
-           if(n-k == m){
-               ans = temp->data;
-           }
-           temp = temp->next;
-           k++;
-       }
-       
-       return ans;
-}
-
