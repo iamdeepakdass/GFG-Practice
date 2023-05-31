41a03d8baf1490fba65823c9bf4f10c78f848a54
Deepak Dass <105672156+iamdeepakdass@users.noreply.github.com>
2023-05-31 23:59:54 +0530

    Updated Solution - GfG to GitHub

--- Easy/Top K Frequent Elements in Array - |/top-k-frequent-elements-in-array-.cpp
+++ Easy/Top K Frequent Elements in Array - |/top-k-frequent-elements-in-array-.cpp
@@ -12,23 +12,27 @@ class Solution {
         // Code here
         int n = nums.size();
         
+        // store frequencies of elements in a map
         map<int, int> mpp;
         for(int i=0; i<n; i++){
             mpp[nums[i]]++;
         }
         
+        // make a vector of pairs and store the value, frequency pair
         vector<pair<int,int>> temp;
         for(auto it: mpp){
             temp.push_back({it.first, it.second});
         }
         
+        // swap pairs and then sort according to the frequencies
         for(int i=0; i<temp.size(); i++){
             swap(temp[i].first, temp[i].second);
         }
-        
         sort(temp.begin(), temp.end());
+        
         int len = temp.size() - 1;
         vector<int> ans;
+        // store values which has maximum frequecies
         while(k--){
             ans.push_back(temp[len--].second);
         }
