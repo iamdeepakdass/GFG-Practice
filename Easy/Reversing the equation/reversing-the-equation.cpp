86cc6ade3fae67b4af8a3ab0c71d2410dc9f052e
Deepak Dass <105672156+iamdeepakdass@users.noreply.github.com>
2023-06-04 17:04:20 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Reversing the equation/reversing-the-equation.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    string reverseEqn (string s)
+        {
+            //code here.
+            stack<char> st;
+            int i = s.size()-1;
+            string ans = "";
+            
+            while(i >= 0){
+                string temp = "";
+                
+                while(s[i] >= '0' && s[i] <= '9'){
+                    temp += s[i--];
+                }
+                reverse(temp.begin(), temp.end());
+                ans += temp;
+                
+                if(s[i] == '+'||s[i] == '-'||s[i] == '*' ||s[i] ==  '/'){
+                    ans += s[i];
+                }
+                i--;
+            }
+            
+            return ans;
+            
+        }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        string s; cin >> s;
+        Solution ob;
+        cout <<ob.reverseEqn (s) << endl;
+    }
+}
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
