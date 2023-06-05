22f4eaee739b3c5ebf8d0d3a068e1535904f9639
Deepak Dass <105672156+iamdeepakdass@users.noreply.github.com>
2023-06-05 22:33:53 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/First element to occur k times/first-element-to-occur-k-times.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int firstElementKTime(int a[], int n, int k)
+    {
+        unordered_map<int, int> mpp;
+        int ans = -1;
+        
+        for(int i=0; i<n; i++){
+            mpp[a[i]]++;
+            if(mpp[a[i]] >= k){
+                return a[i];
+            }
+        }
+        
+        
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    int n, k;
+	    cin >> n >> k;
+	    int a[n];
+	    for(int i = 0;i<n;i++){
+	        cin >> a[i];
+	    }
+	    Solution ob;
+	    cout<<ob.firstElementKTime(a, n, k)<<endl;
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
