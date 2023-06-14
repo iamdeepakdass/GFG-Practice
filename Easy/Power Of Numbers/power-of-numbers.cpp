f52281c44663989c5c0c4e427fbc4da5b832b433
Deepak Dass <105672156+iamdeepakdass@users.noreply.github.com>
2023-06-14 13:07:23 +0530

    Updated Solution - GfG to GitHub

--- Easy/Power Of Numbers/power-of-numbers.cpp
+++ Easy/Power Of Numbers/power-of-numbers.cpp
@@ -8,23 +8,25 @@ class Solution{
     public:
     //You need to complete this fucntion
     
-    long long power(long long N,int R)
+    long long power(int N,int R)
     {
-       
-       long long ans = 1;
-       
-       while(R){
-           if(!(R&1)){
-               N = (N*N)%mod;
-               R = R/2;
-           }
-           else{
-               ans = (N*ans)%mod;
-               R--;
-           }
-       }
+       //Your code here
+        if(N == 0){
+            return 0;
+        }
+        if(R == 0){
+            return 1;
+        }
         
-        return ans%mod;
+        if(R%2 == 0){
+            long long ans = power(N,R/2);
+            return (ans%mod) * (ans%mod)%mod;
+        }
+        else{
+            long long ans = power(N, (R-1)/2);
+            return (N%mod * ans%mod * ans%mod)%mod;
+        }
+        
     }
 
 };
