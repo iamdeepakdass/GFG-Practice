c0ceaca9cf6ff8c1474e3af87fc41274d538053a
Deepak Dass <105672156+iamdeepakdass@users.noreply.github.com>
2023-06-15 13:00:40 +0530

    Updated Solution - GfG to GitHub

--- Medium/Modular Exponentiation for large numbers/modular-exponentiation-for-large-numbers.cpp
+++ Medium/Modular Exponentiation for large numbers/modular-exponentiation-for-large-numbers.cpp
@@ -3,28 +3,27 @@
 using namespace std;
 
 // } Driver Code Ends
+
+#define lli long long int
+
 class Solution
 {
 	public:
 		long long int PowMod(long long int x,long long int n,long long int M)
 		{
 		    // Code here
-		    long long int mod =  M;
-		    if(x == 0){
-            return 0;
-        }
-        if(n == 0){
-            return 1;
-        }
-        
-        if(!(n&1)){
-            long long ans = PowMod(x,n/2,mod);
-            return (ans%mod * ans%mod)%mod;
-        }
-        else{
-            long long ans = PowMod(x,(n-1)/2,mod);
-            return (x%mod * ans%mod * ans%mod)%mod;
-        }
+		    lli ans = 1;
+		    
+		    while(n){
+		        if((n&1)){
+		            ans = (ans*x)%M;
+		        }
+		        
+		        x = (x*x)%M;
+		        n /= 2;
+		    }
+		    
+		    return ans;
 		}
 };
 
