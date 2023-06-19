1d06989f30f65b9f5473fa39cd20b89e849685cd
Deepak Dass <105672156+iamdeepakdass@users.noreply.github.com>
2023-06-19 13:56:45 +0530

    Updated Solution - GfG to GitHub

--- Medium/Rearrange an array with O(1) extra space/rearrange-an-array-with-o1-extra-space.cpp
+++ Medium/Rearrange an array with O(1) extra space/rearrange-an-array-with-o1-extra-space.cpp
@@ -13,12 +13,6 @@ class Solution{
     //Function to rearrange an array so that arr[i] becomes arr[arr[i]]
     //with O(1) extra space.
     void arrange(long long arr[], int n) {
-        // Your code here
-        //vector<int> temp(n);
-        
-        // for(int i=0; i<n; i++){
-        //     temp[i] = arr[i];
-        // }
         
         for(int i=0; i<n; i++){
             arr[i] = arr[i] + (arr[arr[i]]%n)*n;
