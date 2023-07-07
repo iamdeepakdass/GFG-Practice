e716b84829414c14fdfc13d2094047403f848363
Deepak Dass <105672156+iamdeepakdass@users.noreply.github.com>
2023-07-07 23:52:34 +0530

    Updated Solution - GfG to GitHub

--- Hard/Merge Without Extra Space/merge-without-extra-space.cpp
+++ Hard/Merge Without Extra Space/merge-without-extra-space.cpp
@@ -10,24 +10,19 @@ class Solution{
         void merge(long long arr1[], long long arr2[], int n, int m) 
         { 
             // code here 
-            int i=n-1;
-            int j=0; 
+            int i = n-1, j = 0;
             
-            while(i>=0 && j<m){
-                
+            while(i >=0 && j < m){
                 if(arr1[i] > arr2[j]){
-                    swap(arr1[i], arr2[j]);
-                    i--;
-                    j++;
+                    swap(arr1[i--], arr2[j++]);
                 }
-                
                 else{
                     break;
                 }
             }
             
-            sort(arr1, arr1+n);
-            sort(arr2, arr2+m);
+            sort(arr1, arr1 + n);
+            sort(arr2, arr2 + m);
         } 
 };
 
