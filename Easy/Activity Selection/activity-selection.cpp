64e62f5690f974d8ca408d1d54f11ff3a6b1bb4d
Deepak Dass <105672156+iamdeepakdass@users.noreply.github.com>
2023-07-16 15:57:22 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Activity Selection/activity-selection.cpp
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class myComp{
+    public:
+    
+    bool operator() (pair<int,int> a, pair<int,int> b){
+        return a.second < b.second;
+    }
+};
+
+class Solution
+{
+    public:
+    //Function to find the maximum number of activities that can
+    //be performed by a single person.
+    int activitySelection(vector<int> start, vector<int> end, int n)
+    {
+        // Your code here
+        vector<pair<int,int>> intervals;
+        
+        for(int i = 0; i < n; i++){
+            intervals.push_back({start[i], end[i]});
+        }
+        
+        sort(intervals.begin(), intervals.end(), myComp());
+        
+        // for(int i=0; i<n; i++){
+        //     cout<<intervals[i].first<<" "<<intervals[i].second<<"\n";
+        // }
+        
+        //pair<int, int> temp;
+        int cnt = 1;
+        //temp.first = intervals[0].first;
+        int e = intervals[0].second;
+        
+        for(int i=1; i<n; i++){
+            if(intervals[i].first <= e){
+                continue;
+            }
+            //else if(intervals[i].first == intervals[i].second) continue;
+            else{
+                //temp.first = intervals[i].first;
+                //temp.second = intervals[i].second;
+                e = intervals[i].second;
+                cnt++;  
+            } 
+        }
+        
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    
+    //testcases
+    cin >> t;
+    while(t--)
+    {
+        //size of array
+        int n;
+        cin >> n;
+        vector<int> start(n), end(n);
+        
+        //adding elements to arrays start and end
+        for(int i=0;i<n;i++)
+            cin>>start[i];
+        for(int i=0;i<n;i++)
+            cin>>end[i];
+        Solution ob;
+        //function call
+        cout << ob.activitySelection(start, end, n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
