7515c1e6b4683d1934c6bcbe5beae1062253143f
Deepak Dass <105672156+iamdeepakdass@users.noreply.github.com>
2023-07-29 18:38:49 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Longest Palindromic Substring/longest-palindromic-substring.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    string longestPalindrome (string s) {
+        // code here
+        string t = s;
+        reverse(t.begin(), t.end());
+        int n = t.size();
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        int x,y;
+        int maxi = 0;
+        
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<n+1; j++){
+                if(s[i-1] == t[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                    if(dp[i][j] > maxi){
+                        int tIdx = n-j;
+                        if(tIdx + dp[i][j] - 1 == i-1){
+                            x = i;
+                            y = j;
+                            maxi = dp[i][j];
+                        }
+                    }
+                }
+                else dp[i][j] = 0;
+            }
+        }
+        //cout<<maxi<<" maxi\n";
+
+        // cout<<x<<" x "<<y<<" y \n";
+
+        int i = x, j = y;
+        string ans = "";
+        
+        while(i > 0 && j > 0 && dp[i][j] > 0){
+            ans += s[i-1];
+            i--;
+            j--;
+        }
+        
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string S;
+        cin>>S;
+        Solution ob;
+        cout<<ob.longestPalindrome(S)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
