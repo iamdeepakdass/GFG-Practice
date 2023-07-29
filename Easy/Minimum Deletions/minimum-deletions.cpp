5636a71e78898639ad26f6d406e13794e3fd06d3
Deepak Dass <105672156+iamdeepakdass@users.noreply.github.com>
2023-07-29 21:56:25 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Minimum Deletions/minimum-deletions.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    int minimumNumberOfDeletions(string s) { 
+        // code here
+        string t = s;
+        reverse(t.begin(), t.end());
+        int n = s.size();
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<n+1; j++){
+                if(s[i-1] == t[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        
+        return n - dp[n][n];
+    } 
+};
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        string S;
+        cin >> S;
+        Solution obj;
+        cout << obj.minimumNumberOfDeletions(S) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
