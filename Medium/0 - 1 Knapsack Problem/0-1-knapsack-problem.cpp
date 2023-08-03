a5b6320821f647ddc7310e8b574205dd85170816
Deepak Dass <105672156+iamdeepakdass@users.noreply.github.com>
2023-08-03 12:13:31 +0530

    Updated Solution - GfG to GitHub

--- Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
+++ Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -13,26 +13,26 @@ class Solution
     int knapSack(int W, int wt[], int val[], int n) 
     { 
        
-       vector<vector<int>> t(n+1, vector<int>(W+1,-1));
+        vector<vector<int>> t(n+1, vector<int>(W+1, 0));
        
-       rep(i,0,n+1){
-           rep(j,0, W+1){
-               if(i == 0 || j == 0){
-                   t[i][j] = 0;
-               }
-           }
-       }
+        // rep(i,0,n+1){
+        //     rep(j,0, W+1){
+        //         if(i == 0 || j == 0){
+        //             t[i][j] = 0;
+        //         }
+        //     }
+        // }
        
-       rep(i,1,n+1){
-           rep(j,1, W+1){
-               if(wt[i-1] <= j){
-                   t[i][j] = max(val[i-1] + t[i-1][j-wt[i-1]] , t[i-1][j]);
-               }
-               else t[i][j] = t[i-1][j];
-           }
-       }
+        rep(i,1,n+1){
+            rep(j,1, W+1){
+                if(wt[i-1] <= j){
+                    t[i][j] = max(val[i-1] + t[i-1][j-wt[i-1]] , t[i-1][j]);
+                }
+                else t[i][j] = t[i-1][j];
+            }
+        }
        
-       return t[n][W];
+        return t[n][W];
     }
 };
 
