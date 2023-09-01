929f89eaaef437580a0aae2f259a8e759c576699
Deepak Dass <105672156+iamdeepakdass@users.noreply.github.com>
2023-09-01 17:34:58 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Leftmost and rightmost nodes of binary tree/leftmost-and-rightmost-nodes-of-binary-tree.cpp
@@ -0,0 +1,162 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+void printCorner(Node *root);
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if(currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if(currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+
+int main() {
+    int t;
+    string tc;
+    getline(cin, tc);
+    t=stoi(tc);
+    while(t--)
+    {
+        string s ,ch;
+        getline(cin, s);
+        Node* root = buildTree(s);
+
+        printCorner(root);
+        cout<<endl;
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+// } Driver Code Ends
+
+
+/* Function to print corner node at each level */
+
+/*
+Structure of a node is as following
+struct Node
+{
+     int data;
+     struct Node* left;
+     struct Node* right;
+};
+*/
+
+void printCorner(Node *root)
+{
+
+// Your code goes here  
+    
+    #define push push_back
+    #define pop pop_front
+    deque<pair<Node , int>> q;
+    q.push(make_pair(*root, 1));
+    int i=1;
+    deque<pair<Node , int>> q1;
+    vector<int> v;
+    while(!q.empty()){
+        Node node = q.front().first;
+        int level = q.front().second;
+        if ((node.left) != nullptr) q1.push(make_pair(*(node.left), level+1));
+        if ((node.right) != nullptr)q1.push(make_pair(*(node.right), level+1));
+        if (i and q.size()!=1) {cout<<((node).data)<<" "; i=0;}
+        if (q.size()==1){
+            cout<<((node.data))<<" ";
+            i=1;
+        }
+        q.pop();
+        if (q.size()==0) {
+            q=q1;
+            q1.clear();
+        }
+    }
+}
