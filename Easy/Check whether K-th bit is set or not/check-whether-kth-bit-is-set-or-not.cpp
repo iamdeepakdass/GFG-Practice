1998b657ad1b7e0fe29e7ec02f6479c8a5bedb84
Deepak Dass <105672156+iamdeepakdass@users.noreply.github.com>
2023-10-29 18:44:18 +0530

    Updated Solution - GfG to GitHub

--- Easy/Check whether K-th bit is set or not/check-whether-kth-bit-is-set-or-not.cpp
+++ Easy/Check whether K-th bit is set or not/check-whether-kth-bit-is-set-or-not.cpp
@@ -17,9 +17,7 @@ class Solution
     {
         // Your code here
         // It can be a one liner logic!! Think of it!!
-        if(n&(1<<k)){
-            return true;
-        }
+        if(n & (1<<k)) return true;
         return false;
     }
 };
